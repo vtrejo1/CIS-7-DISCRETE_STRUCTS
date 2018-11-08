53893f032fe6ae2b4d68382e1f10b08ec1fcec3c
Solo <vtrejo1@student.rccd.edu>
2018-11-07 19:55:16 -0800

    () added

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,88 +1,3 @@
-/*
-#include <iostream>
-#include <stdio.h>
-#include <ctype.h>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-	string userInput;
-	bool isStatement = false;
-	bool successXD = true;
-	bool impTest = false;
-	cout << "Enter Statement ";
-	getline(cin, userInput);
-	cout << endl;
-
-		for (char &c : userInput)
-		{
-			if (impTest == true)
-			{
-				if (c == '>')
-				{
-					isStatement = false;
-					impTest = false;
-					break;
-				}
-				else
-					successXD = false;
-				break;
-				
-			}
-			if (isalpha(c) == true)
-			{
-				if (isStatement == isalpha(c))
-				{
-					successXD = false;
-					continue;
-				}
-				else
-					isStatement = true;
-			}
-			else if ((c == '^') || (c == 'v'))
-			{
-				if (isStatement)
-				{
-					isStatement = false;
-				}
-				else
-				{
-					successXD = false;
-					continue;
-				}
-			}
-			else if (c == '!')
-			{
-				if (isStatement)
-					isStatement = false;
-			}
-			else if (c == '-')
-			{
-				impTest = true;
-				continue;
-			}
-			else if (c == ' ')
-				continue;
-			else
-			{
-				successXD = false;
-				break;
-			}
-
-		}
-		if (successXD)
-			cout << "Is a wff" << endl;
-		else
-			cout << "Is not a wff" << endl;
-	
-	
-	system("pause");
-	return 0;
-}*/
-
-//reg expressions ftw :3
 #include <string>
 #include <regex>
 #include <iostream>
@@ -96,10 +11,33 @@ int main()
         getline(cin, userInput);
         cout << endl;
 
-        if(regex_match(userInput, regex("(\\!?[A-Z])((v|\\^|\\-\\>)(\\!?[A-Z]))+")))
-            cout << "Is a WFF" << endl;
+        if(regex_match(userInput, regex("\\!?(\\(+\\!?)*([A-Z])( ((v|V|\\^|\\-\\>) (\\!?(\\(+\\!?)*[A-Z](\\)*))))+")))
+        {
+        	int pCount = 0;
+        	for(char& c : userInput)
+        	{
+        		if(c == '(')
+        			pCount++;
+        		if(c == ')')
+        			pCount--;
+
+        		if(pCount == -1)
+        		{
+        			cout << "Is NOT a WFF due to a missing open parenthesis" << endl;
+        			break;
+        		}
+        	}
+
+        	if(pCount == 0)
+        	{
+        		cout << "Is a WFF" << endl;
+        	}
+            if(pCount > 0)
+            	cout << "Is NOT a WFF due to unclosed parenthesis" << endl;
+        }
         else
             cout << "Is NOT a WFF" << endl;
+        
 
 
         system("pause");
